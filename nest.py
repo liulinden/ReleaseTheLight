42c098ced65437a1977b94327973c712264cda31
Linden <50274080+liulinden@users.noreply.github.com>
2025-03-08 16:29:12 -0800

    switched back nest charge speed

--- nest.py
+++ nest.py
@@ -77,7 +77,7 @@ class Nest:
         self.maxCharge=self.maxHealth/3+100
         self.visualCharge=self.maxCharge
         self.charge=self.maxCharge*0.5
-        self.chargeRate=self.maxCharge/30000
+        self.chargeRate=self.maxCharge/10000
         self.charging=(0,0,0)
         if self.nestType=="White":
             self.charging=(1,0,0)
