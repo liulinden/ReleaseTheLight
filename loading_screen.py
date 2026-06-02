21f789922b665d42b908838df4255ab3968472d7
MichaelLesirge <michael.lesirge@gmail.com>
2026-06-02 14:57:02 -0700

    smooth loading  bar

--- loading_screen.py
+++ loading_screen.py
@@ -9,7 +9,7 @@ ASSETS = pathlib.Path("assets")
 FPS = 15 # low FPS
 
 class TitleSpinner(pygame.sprite.Sprite):
-    def __init__(self, frames: list[pygame.Surface], fps):
+    def __init__(self, frames: list[pygame.Surface], fps: int) -> None:
         super().__init__()
 
         self.frames = frames
@@ -19,9 +19,12 @@ class TitleSpinner(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
         self.previous_time = pygame.time.get_ticks()
+        self.set_fps(fps)
+    
+    def set_fps(self, fps: int) -> None:
         self.frame_duration = 1000 // fps
     
-    def update(self):
+    def update(self) -> None:
         current_time = pygame.time.get_ticks()
         if current_time - self.previous_time >= self.frame_duration:
             self.previous_time = current_time
@@ -29,29 +32,42 @@ class TitleSpinner(pygame.sprite.Sprite):
             self.image = self.frames[self.frame_index]
 
 class LoadingBar(pygame.sprite.Sprite):
-    def __init__(self, size: tuple[int, int]):
+    def __init__(self, size: tuple[int, int], percentage_per_second: float = 1) -> None:
         super().__init__()
         
         self.image = pygame.Surface(size, pygame.SRCALPHA)
         self.rect = self.image.get_rect()
+
+        self.displayed_progress = 0
         self.set_progress(0)
-    
-    def set_progress(self, progress: float):
+        self.set_percentage_per_second(percentage_per_second)
+
+    def set_percentage_per_second(self, percentage_per_second: float) -> None:
+        self.max_progress_change_per_frame = percentage_per_second / FPS
+
+    def set_progress(self, progress: float) -> None:
         self.progress = progress
+    
+    def get_progress(self) -> float:
+        return self.progress
+    
+    def get_display_progress(self) -> float:
+        return self.displayed_progress
+
+    def update(self) -> None:
+
+        self.displayed_progress = min(self.progress, self.displayed_progress + self.max_progress_change_per_frame)
 
         outer_rect = self.image.get_rect()
         pygame.draw.rect(self.image, (0,0,0,125), outer_rect, border_radius=10)
         pygame.draw.rect(self.image, (255,255,255,255), outer_rect, width=3, border_radius=10)
 
         inner_rect = outer_rect.inflate(-15, -15)
-        inner_rect.width *= self.progress
+        inner_rect.width *= self.displayed_progress
         pygame.draw.rect(self.image, (255,255,255,245), inner_rect, border_radius=5)
 
-    def get_progress(self):
-        return self.progress
-
 class LoadingScreen:
-    def __init__(self, surface: pygame.Surface, *, _queue=None, start_progress=0, end_progress=1):
+    def __init__(self, surface: pygame.Surface, *, _queue: Queue = None, start_progress=0.0, end_progress=1.0) -> None:
 
         self.surface = surface
 
@@ -72,7 +88,7 @@ class LoadingScreen:
             frames.append(frame)
         return frames
 
-    def _gradient(self) -> pygame.Surface:
+    def _gradient_surface(self) -> pygame.Surface:
         gradient = pygame.image.load(ASSETS / "VignetteGradientTitle.webp").convert_alpha()
         size = int(self.surface.get_height() * 1.7)
         gradient = pygame.transform.scale(gradient, (size, size))
@@ -99,19 +115,18 @@ class LoadingScreen:
         return [self.subsection(start_at, end_at) for start_at, end_at in zip(subsections, subsections[1:] + (1.0,))]
 
     def run(self) -> bool:
-        going = True
-
         clock = pygame.time.Clock()
 
-        # font = pygame.font.SysFont("Arial", self.surface.get_height() // 20)
 
         title_spinner = TitleSpinner(self._title_frames(), 12)
         title_spinner.rect.center = (self.surface.get_width() // 2, self.surface.get_height() // 2)
 
-        loading_bar = LoadingBar((title_spinner.rect.width * 0.9, self.surface.get_height() // 50))
-        loading_bar.rect.center = (self.surface.get_width() // 2, title_spinner.rect.bottom - title_spinner.rect.height * 0.18)
+        loading_bar = LoadingBar((title_spinner.rect.width * 0.9, self.surface.get_height() // 50), percentage_per_second=0.1)
+        loading_bar.rect.center = (self.surface.get_width() // 2, int(title_spinner.rect.bottom - title_spinner.rect.height * 0.18))
 
-        gradient = self._gradient()
+        gradient = self._gradient_surface()
+
+        font = pygame.font.SysFont("Arial", loading_bar.rect.height // 2, bold=False)
 
         sprites = pygame.sprite.Group(title_spinner, loading_bar)
 
@@ -120,6 +135,7 @@ class LoadingScreen:
 
         # print(f"Loading screen [{self.start_progress:.2f} - {self.end_progress:.2f}] started.")
 
+        going = True
         while going and progress < self.end_progress:
 
             self.surface.fill("black")
@@ -134,6 +150,10 @@ class LoadingScreen:
             
             sprites.update()
             sprites.draw(self.surface)
+
+            # text_surface = font.render(f"{progress:.0%}", True, (220,220,255))
+            # text_rect = text_surface.get_rect(midtop=loading_bar.rect.midbottom)
+            # self.surface.blit(text_surface, text_rect)
 
             if not self.queue.empty():
                 progress = self.queue.get()
