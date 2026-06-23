b36c64bc8a25a4a4c5098c25cb45ce4824dee85a
Linden <50274080+liulinden@users.noreply.github.com>
2026-06-23 01:00:08 -0700

    elements start

---
+++ bouncepads.py
@@ -0,0 +1,14 @@
+import pygame
+from elements import Element
+
+class Bouncepad(Element):
+    
+
+    def __init__(self, defaultZooms, x, y, size):
+        super().__init__(x, y, x, y+size/2, size/4, True, False, defaultZooms)
+    
+    def getHitboxSurface(self, zoom):
+        pass
+    
+    def draw(self, zoom):
+        pass
